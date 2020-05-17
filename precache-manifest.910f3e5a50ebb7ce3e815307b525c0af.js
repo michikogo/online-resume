02114b6b31e891980b45c462b3e361d7bd74f7f4
michikogo <37466954+michikogo@users.noreply.github.com>
2020-05-18 00:26:08 +0800

    Updates

---
+++ precache-manifest.910f3e5a50ebb7ce3e815307b525c0af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13b58b1943ceac7e277814e3eb8548d",
+    "url": "/online-resume/index.html"
+  },
+  {
+    "revision": "a930aa88828a63ae0ab9",
+    "url": "/online-resume/static/css/2.3c4e6f21.chunk.css"
+  },
+  {
+    "revision": "f983a4ba666042d8f5b0",
+    "url": "/online-resume/static/css/main.24427a4f.chunk.css"
+  },
+  {
+    "revision": "a930aa88828a63ae0ab9",
+    "url": "/online-resume/static/js/2.6d57c45e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/online-resume/static/js/2.6d57c45e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f983a4ba666042d8f5b0",
+    "url": "/online-resume/static/js/main.4332cfb3.chunk.js"
+  },
+  {
+    "revision": "8771e209351c48d93890",
+    "url": "/online-resume/static/js/runtime-main.687fd3f9.js"
+  },
+  {
+    "revision": "488aae5bf2a25eefb9fd949f46d89d53",
+    "url": "/online-resume/static/media/WVKj7E.488aae5b.jpg"
+  }
+]);
