166b16eff81f70c1c851ed0461c0dd03d1d32c25
michikogo <37466954+michikogo@users.noreply.github.com>
2020-05-18 01:39:17 +0800

    added fonts

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import './index.css';
 
-import "antd/dist/antd.css";
-import { Layout, Breadcrumb } from "antd";
+// import "antd/dist/antd.css";
+import { Layout, Typography } from "antd";
+
 import Header from '../common/header';
 import Footer from '../common/footer';
 
 const Home = () => {
     const { Content } = Layout;
+    const { Title } = Typography;
+
     return (
         <Layout>
             <Header />
-            <Content className="site-layout home-layout" style={{ padding: '0 50px' }}>
-                <Breadcrumb style={{ margin: '16px 0' }}>
-                    <Breadcrumb.Item>Home</Breadcrumb.Item>
-                    <Breadcrumb.Item>List</Breadcrumb.Item>
-                    <Breadcrumb.Item>App</Breadcrumb.Item>
-                </Breadcrumb>
-                <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
-                    Content
-                </div>
+            <Content className="home-layout home-name">
+                <p>Michiko Young Go</p>
             </Content>
             <Footer />
         </Layout>
