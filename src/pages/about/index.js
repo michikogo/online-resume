7bfb71978ed2c5f67f18ecd44e6889f76b82dece
michikogo <37466954+michikogo@users.noreply.github.com>
2020-05-19 15:06:24 +0800

    change color and font

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -15,8 +15,22 @@ const About = () => {
     return (
         <Layout>
             <Header />
-            <Content className="home-layout">
-                <Row className="home-row">
+            <Content>
+                {/* Hello Page */}
+                <Row className="home-hello">
+                    <Col lg={24}>
+                        <FadeIn delay="400" transitionDuration="1000">
+                            <p className="home-hello-name">Michiko Young Go</p>
+                        </FadeIn>
+                    </Col>
+                    <Col lg={24}>
+                        <FadeIn delay="600" transitionDuration="1000">
+                            <p className="home-hello-course">B.S. Computer Science</p>
+                        </FadeIn>
+                    </Col>
+                </Row>
+                {/* Profile - Name, Age, Course, Location */}
+                <Row className="home-profile">
                     <Col lg={24}>
                         <FadeIn delay="400" transitionDuration="1000">
                             <p className="home-name">Michiko Young Go</p>
@@ -28,7 +42,10 @@ const About = () => {
                         </FadeIn>
                     </Col>
                 </Row>
-            </Content>
+
+                {/* Experiences  and Involvement */}
+                {/* Skills */}
+            </Content >
             <Footer />
         </Layout >
     )
