90d1593c5f70225d66b245af9ba8bcc8cbcebeaf
michikogo <37466954+michikogo@users.noreply.github.com>
2020-05-20 16:41:11 +0800

    skills page added

--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,7 @@
 import background from './WVKj7E.jpg';
-
+import scrible from './scrible.png';
+import scrible2 from './scrible2.png';
 export {
-    background
+    background,
+    scrible, scrible2
 }
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
+import { scrible, scrible2 } from '../../images';
 
-import FadeIn from 'react-fade-in';
+import Fade from 'react-reveal/Fade';
+import Jump from 'react-reveal/Jump'
+// import FadeIn from 'react-fade-in';
 // import "antd/dist/antd.css";
 import { Layout, Row, Col } from "antd";
+import { DownOutlined } from '@ant-design/icons';
 
 import Header from '../../common/header';
 import Footer from '../../common/footer';
 
 const About = () => {
     const { Content } = Layout;
-    // const { Title } = Typography;
+
+    const [skills, setSkills] = useState([
+        { number: "01. ", skill: "Programming Languages (C, C++, Java, MySQL)" },
+        { number: "02. ", skill: "Programming Languages (C, C++, Java, MySQL)" },
+        { number: "03. ", skill: "Programming Languages (C, C++, Java, MySQL)" },
+    ])
+
+    const [skills2, setSkills2] = useState([
+        { number: "04. ", skill: "Programming Languages (C, C++, Java, MySQL)" },
+        { number: "05. ", skill: "Programming Languages (C, C++, Java, MySQL)" },
+        { number: "06. ", skill: "Programming Languages (C, C++, Java, MySQL)" },
+    ])
 
     return (
         <Layout>
@@ -19,32 +34,64 @@ const About = () => {
                 {/* Hello Page */}
                 <Row className="home-hello">
                     <Col lg={24}>
-                        <FadeIn delay="400" transitionDuration="1000">
+                        <Fade bottom big oposite delay={800} duration={1000}>
                             <p className="home-hello-name">Michiko Young Go</p>
-                        </FadeIn>
+                        </Fade>
                     </Col>
                     <Col lg={24}>
-                        <FadeIn delay="600" transitionDuration="1000">
+                        <Fade bottom big oposite delay={1000} duration={1000}>
                             <p className="home-hello-course">B.S. Computer Science</p>
-                        </FadeIn>
+                        </Fade>
+                    </Col>
+                    <Col lg={24}>
+                        <img src={scrible} alt="scrible" className="home-hello-image" />
                     </Col>
                 </Row>
-                {/* Profile - Name, Age, Course, Location */}
-                <Row className="home-profile">
+                <Row className="home-hello-down-row">
+                    <Jump forever={true}>
+                        <DownOutlined className="home-hello-down" />
+                    </Jump>
+                </Row>
+                {/* Skills */}
+                <img src={scrible2} alt="scrible2" className="home-skills-image" />
+                <Row className="home-skills">
                     <Col lg={24}>
-                        <FadeIn delay="400" transitionDuration="1000">
-                            <p className="home-name">Michiko Young Go</p>
-                        </FadeIn>
+                        <p className="home-skills-name"> Skills </p>
                     </Col>
-                    <Col lg={24}>
-                        <FadeIn delay="600" transitionDuration="1000">
-                            <p className="home-course">B.S. Computer Science</p>
-                        </FadeIn>
-                    </Col>
+                    <Row>
+                        <Col lg={12}>
+                            {
+                                skills !== ' '
+                                    ? skills.map((data, i) => {
+                                        return (
+                                            <Row key={i} className="home-skills-content">
+                                                <p className="home-skills-number">{data.number}</p>
+                                                <p className="home-skills-skill">{data.skill}</p>
+                                            </Row>
+                                        )
+                                    })
+                                    : "No Data"
+                            }
+                        </Col>
+                        <Col lg={12}>
+                            {
+                                skills2 !== ' '
+                                    ? skills2.map((data, i) => {
+                                        return (
+                                            <Row key={i} className="home-skills-content">
+                                                <p className="home-skills-number">{data.number}</p>
+                                                <p className="home-skills-skill">{data.skill}</p>
+                                            </Row>
+                                        )
+                                    })
+                                    : "No Data"
+                            }
+                        </Col>
+                    </Row>
                 </Row>
 
+                {/* Profile - Name, Age, Course, Location */}
                 {/* Experiences  and Involvement */}
-                {/* Skills */}
             </Content >
             <Footer />
         </Layout >
