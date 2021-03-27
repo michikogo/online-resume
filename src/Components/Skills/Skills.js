ac0e42b054be3e60c602c7b51d73975c675ab9c8
michikogo <37466954+michikogo@users.noreply.github.com>
2021-03-28 04:04:12 +0800

    add skills component

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -4,8 +4,13 @@ import { Container, Row, Col } from "react-bootstrap";
 const Skills = () => {
   return (
     <Container fluid className="skills-background">
-      <Row className="skills-row">
+      <Row className="skills-title">
         <Col>Skills</Col>
+      </Row>
+      <Row className="skills-row">
+        <Col>
+          <Row>asd</Row>
+        </Col>
       </Row>
     </Container>
   );
