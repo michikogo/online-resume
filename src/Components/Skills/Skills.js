37a2d6a9f744f109fcdce158d1bc5e4739650a52
michikogo <37466954+michikogo@users.noreply.github.com>
2021-03-29 16:25:05 +0800

    created content

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -1,7 +1,22 @@
 import "./index.css";
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 const Skills = () => {
+  const [knowledgable, setKnowledgable] = useState([
+    "UI/UX",
+    "Web Application Development",
+    "Android Development",
+    "Game Development",
+    "Object-Oriented Programming",
+    "MVC Framework",
+    "Project Management Frameworks",
+    "Git",
+    "OLAP Dashboard",
+    "Enterprise Resource Planning (ERP)",
+    "Machine Learning",
+    "Node.js",
+  ]);
   return (
     <Container fluid className="skills-background">
       <Row className="skills-title">
@@ -9,7 +24,15 @@ const Skills = () => {
       </Row>
       <Row className="skills-row">
         <Col>
-          <Row>asd</Row>
+          <Row>
+            {knowledgable.map((knowledge) => (
+              <Col className="skills-box">
+                <Row className="skills-text">
+                  <Col>{knowledge}</Col>
+                </Row>
+              </Col>
+            ))}
+          </Row>
         </Col>
       </Row>
     </Container>
