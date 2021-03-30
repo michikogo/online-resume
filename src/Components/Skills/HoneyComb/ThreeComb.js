51ba5eee9959bbf12d636654af2d1d25a27213d8
michikogo <37466954+michikogo@users.noreply.github.com>
2021-03-30 16:23:03 +0800

    created combs

--- src/Components/Skills/HoneyComb/ThreeComb.js
+++ src/Components/Skills/HoneyComb/ThreeComb.js
@@ -2,51 +2,63 @@ import React from "react";
 import Hexagon from "react-hexagon";
 import { Col, Row } from "react-bootstrap";
 
-const ThreeComb = () => {
+const ThreeComb = ({ data1, data2, data3 }) => {
   return (
     <Row className="justify-content-sm-center" style={{ marginTop: "-15px" }}>
-      <Col
-        sm={{ span: 3 }}
-        style={{
-          // marginLeft: "59px",
-          // marginTop: "-22px",
-          padding: "0px 1vh",
-        }}
-      >
-        <Hexagon
-          style={{ stroke: "#42873f" }}
-          backgroundImage="img/red-panda.jpg"
-          href="http://espen.codes/"
-        />
-      </Col>
-      <Col
-        sm={3}
-        style={{
-          // marginLeft: "59px",
-          // marginTop: "-22px",
-          padding: "0px 1vh",
-        }}
-      >
-        <Hexagon
-          style={{ stroke: "#42873f" }}
-          backgroundImage="img/red-panda.jpg"
-          href="http://espen.codes/"
-        />
-      </Col>
-      <Col
-        sm={3}
-        style={{
-          // marginLeft: "59px",
-          // marginTop: "-22px",
-          padding: "0px 1vh",
-        }}
-      >
-        <Hexagon
-          style={{ stroke: "#42873f" }}
-          backgroundImage="img/red-panda.jpg"
-          href="http://espen.codes/"
-        />
-      </Col>
+      {data1 && (
+        <Col
+          sm={{ span: 3 }}
+          style={{
+            // marginLeft: "59px",
+            // marginTop: "-22px",
+            padding: "0px 1vh",
+          }}
+        >
+          <Hexagon
+            style={{ stroke: "#42873f" }}
+            backgroundImage={
+              require(`../../../Assets/Logo/${data1.image}`).default
+            }
+            href="http://espen.codes/"
+          />
+        </Col>
+      )}
+      {data2 && (
+        <Col
+          sm={3}
+          style={{
+            // marginLeft: "59px",
+            // marginTop: "-22px",
+            padding: "0px 1vh",
+          }}
+        >
+          <Hexagon
+            style={{ stroke: "#42873f" }}
+            backgroundImage={
+              require(`../../../Assets/Logo/${data2.image}`).default
+            }
+            href="http://espen.codes/"
+          />
+        </Col>
+      )}
+      {data3 && (
+        <Col
+          sm={3}
+          style={{
+            // marginLeft: "59px",
+            // marginTop: "-22px",
+            padding: "0px 1vh",
+          }}
+        >
+          <Hexagon
+            style={{ stroke: "#42873f" }}
+            backgroundImage={
+              require(`../../../Assets/Logo/${data3.image}`).default
+            }
+            href="http://espen.codes/"
+          />
+        </Col>
+      )}
     </Row>
   );
 };
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -4,26 +4,28 @@ import { Container, Row, Col } from "react-bootstrap";
 import TwoComb from "./HoneyComb/TwoComb";
 import ThreeComb from "./HoneyComb/ThreeComb";
 import FourComb from "./HoneyComb/FourComb";
+import { isCompositeComponentWithType } from "react-dom/test-utils";
 // import { bootstrap } from "../../Assets";
 // import OddRow from "./HoneyComb/OddRow";
 // import EvenRow from "./HoneyComb/EvenRow";
 
 const Skills = () => {
   const [programming, setProgramming] = useState([
-    "C",
-    "C#",
-    "Java",
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "jQuery",
-    "React Hooks",
-    " Vue",
-    "Angular",
-    "Python",
-    "Firebase",
-    "MySQL",
-    "MongoDB",
+    { id: 1, name: "C", image: "c.png" },
+    { id: 2, name: "C#", image: "c#.png" },
+    { id: 3, name: "Java", image: "java.png" },
+    { id: 4, name: "Python", image: "python.png" },
+    { id: 5, name: "HTML", image: "html.png" },
+    { id: 6, name: "CSS", image: "css.png" },
+    { id: 7, name: "JavaScript", image: "javascript.png" },
+    // { id: 8, name: "jQuery", image: "jquery.png" },
+    { id: 9, name: "React Hooks", image: "react.png" },
+    { id: 10, name: "Vue.js", image: "vue.png" },
+    { id: 11, name: "Angular", image: "angular.png" },
+    { id: 12, name: "Handlebars", image: "handlebars.png" },
+    { id: 13, name: "Firebase", image: "firebase.png" },
+    { id: 14, name: "mySQL", image: "mysql.png" },
+    { id: 15, name: "mongoDB", image: "mongodb.png" },
   ]);
 
   console.log("programming: " + programming.length);
@@ -43,7 +45,7 @@ const Skills = () => {
     "PowerBI",
   ]);
   console.log("applications: " + applications.length);
-
+  // console.log(programming[0].image);
   return (
     <Container fluid className="skills-background">
       <Row className="skills-title">
@@ -53,11 +55,25 @@ const Skills = () => {
         <Col>
           <Row>
             <Col sm={6}>
-              <TwoComb />
-              <ThreeComb />
-              <FourComb />
-              <ThreeComb />
-              <TwoComb />
+              <TwoComb data1={programming[0]} data2={programming[1]} />
+              <ThreeComb
+                data1={programming[2]}
+                data2={programming[3]}
+                data3={programming[4]}
+              />
+              <FourComb
+                data1={programming[5]}
+                data2={programming[6]}
+                data3={programming[7]}
+                data4={programming[8]}
+              />
+
+              <ThreeComb
+                data1={programming[9]}
+                data2={programming[10]}
+                data3={programming[11]}
+              />
+              <TwoComb data1={programming[12]} data2={programming[13]} />
             </Col>
             <Col sm={6}>
               Description is here
