9e9ee44be919b93f2c85553cf7f40445487fa2b0
michikogo <37466954+michikogo@users.noreply.github.com>
2021-04-10 04:36:53 +0800

    moved content to different js file

---
+++ src/Sections/Welcome/Components/WelcomeContent.js
@@ -0,0 +1,34 @@
+import { Row, Col, Image } from "react-bootstrap";
+
+const WelcomeContent = () => {
+  return (
+    <>
+      <Row className="welcome-row">
+        <Col sm={4} className="welcome-image-col">
+          <Image
+            className="welcome-image"
+            fluid
+            src={require("../../../Assets/Image/hello.gif").default}
+            alt="I just want to say hi 😢"
+          />
+        </Col>
+        <Col className="welcome-intro-col">
+          <Row>
+            <Col>
+              <p className="welcome-im">Hello! I'm</p>
+              <p className="welcome-name">Michiko Young Go</p>
+              <p className="welcome-email">michikogo98@gmail.com</p>
+              <p className="welcome-paragraph">
+                Hey you! <br /> I am a software developer that focuses on
+                fullstack web development and sometimes mobile development, on
+                my free time I love exploring what the world is there to offer.
+              </p>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default WelcomeContent;
--- src/Sections/Welcome/Welcome.js
+++ src/Sections/Welcome/Welcome.js
@@ -1,34 +1,12 @@
 import "./index.css";
-import { hello } from "../../Assets/index";
-import { Col, Container, Image, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+
+import WelcomeContent from "./Components/WelcomeContent";
 
 const Welcome = () => {
   return (
     <Container fluid className="welcome-background">
-      <Row className="welcome-row">
-        <Col sm={4} className="welcome-image-col">
-          <Image
-            className="welcome-image"
-            fluid
-            src={hello}
-            alt="Secret Message: Welcome to my Resume 😊"
-          />
-        </Col>
-        <Col className="welcome-intro-col">
-          <Row>
-            <Col>
-              <p className="welcome-im">Hello! I'm</p>
-              <p className="welcome-name">Michiko Young Go</p>
-              <p className="welcome-email">michikogo98@gmail.com</p>
-              <p className="welcome-paragraph">
-                Hey you! <br /> I am a software developer that focuses on
-                fullstack web development and sometimes mobile development, on
-                my free time I love exploring what the world is there to offer.
-              </p>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <WelcomeContent />
     </Container>
   );
 };
