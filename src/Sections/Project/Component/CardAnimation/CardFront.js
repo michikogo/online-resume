f03906bf0fc8516110ec814f00b984a5ac2fb74b
michikogo <37466954+michikogo@users.noreply.github.com>
2021-04-09 17:33:08 +0800

    update

--- src/Sections/Project/Component/CardAnimation/CardFront.js
+++ src/Sections/Project/Component/CardAnimation/CardFront.js
@@ -4,22 +4,28 @@ import { Col, Row, Image, Badge } from "react-bootstrap";
 const CardFront = ({ project }) => {
   return (
     <div class="flip-card-front">
-      <Row>
-        {/* TITLE & TYPE*/}
+      <Row className="project-front-row">
         <Col>
-          <p className="project-modal-name">{project.name}</p>
-          <p className="project-modal-type">{project.type}</p>
-        </Col>
-      </Row>
+          <Row>
+            {/* TITLE & TYPE*/}
+            <Col>
+              <p className="project-modal-name">{project.name}</p>
+              <p className="project-modal-type">{project.type}</p>
+            </Col>
+          </Row>
 
-      <Row>
-        {/* IMAGE */}
-        <Col sm={12} className="project-modal-image-col">
-          <Image
-            fluid
-            src={require(`../../../../Assets/Image/${project.image}`).default}
-            className="project-modal-image"
-          />
+          <Row>
+            {/* IMAGE */}
+            <Col sm={12} className="project-modal-image-col">
+              <Image
+                fluid
+                src={
+                  require(`../../../../Assets/Image/${project.image}`).default
+                }
+                className="project-modal-image"
+              />
+            </Col>
+          </Row>
         </Col>
       </Row>
     </div>
