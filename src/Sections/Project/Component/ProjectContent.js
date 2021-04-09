27c36101d9342eb8e35f69d2ce28b41b354cae9f
michikogo <37466954+michikogo@users.noreply.github.com>
2021-04-09 17:07:53 +0800

    update

--- src/Sections/Project/Component/ProjectContent.js
+++ src/Sections/Project/Component/ProjectContent.js
@@ -1,23 +1,72 @@
 import "./buttonAnimate.css";
-import { Col, Row } from "react-bootstrap";
+import { useState } from "react";
+import { Col, Row, Nav } from "react-bootstrap";
 import SpecificProject from "./SpecificProject";
 import CardAnimation from "./CardAnimation/CardAnimation";
 
-const Content = ({ projects }) => {
+const Content = ({ webProjects, mobileProjects, otherProjects }) => {
+  const [activeNav, setActiveNav] = useState("webProjects");
   return (
     <>
+      {/* TITLE */}
       <Row className="project-title-row">
         <Col>
           <span className="project-title">Projects</span>
         </Col>
       </Row>
       <Row className="project-row">
-        {projects.map((project) => (
-          <div>
-            <CardAnimation project={project} />
-          </div>
-        ))}
+        <Col>
+          {/* NAV BAR */}
+          <Nav justify variant="tabs" defaultActiveKey={activeNav}>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="webProjects"
+                onClick={() => setActiveNav("webProjects")}
+              >
+                Web Applications
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="mobileProjects"
+                onClick={() => setActiveNav("mobileProjects")}
+              >
+                Mobile Applications
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link
+                eventKey="otherProjects"
+                onClick={() => setActiveNav("otherProjects")}
+              >
+                Others
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          {/* ANIMATED CARD */}
+          <Row className="project-tab-row">
+            {activeNav == "webProjects" &&
+              webProjects.map((webProject) => (
+                <Col sm={6}>
+                  <CardAnimation project={webProject} />
+                </Col>
+              ))}
+            {activeNav == "mobileProjects" &&
+              mobileProjects.map((mobileProject) => (
+                <Col sm={6}>
+                  <CardAnimation project={mobileProject} />
+                </Col>
+              ))}
+            {activeNav == "otherProjects" &&
+              otherProjects.map((otherProject) => (
+                <Col sm={6}>
+                  <CardAnimation project={otherProject} />
+                </Col>
+              ))}
+          </Row>
+        </Col>
       </Row>
+      {/* CARD WITHOUT ANIMATION */}
       {/* <Row className="project-row">
         {projects.map((project) => (
           <Col sm={12} lg={5} className="project-specific-project ">
--- src/Sections/Project/Project.js
+++ src/Sections/Project/Project.js
@@ -6,26 +6,19 @@ import Rodal from "rodal";
 import ProjectContent from "./Component/ProjectContent";
 
 const Education = () => {
-  const projects = [
+  const webProjects = [
     {
       id: 1,
       image: "bakeryMNL.PNG",
       name: "BakeryMNL",
       type: "eCommerce Website",
-      language: "ReactJs/ JSON",
       summary:
         "BakeryMNL is a simple responsive site that uses ReactJs and JSON Server. This site eCommerce site was created to enhance my skills in both React Hooks and JSON Server.",
-      tags: [
-        "React",
-        "JSON Server",
-        "react-bootstrap",
-        "Responsive",
-        "Wireframe",
-      ],
       features: [
         "Simple responsive website that allows users to buy products, show their total bill, add more products and place their information for shipping or pickup",
         "The website was created to practice react and react-bootstrap, and JSON server.",
       ],
+      tags: ["React", "JSON Server", "react-bootstrap", "Responsive"],
       isAddress: true,
       address: "https://michikogo.github.io/BakeryMNL/#/",
     },
@@ -34,15 +27,14 @@ const Education = () => {
       image: "HUMALIT.PNG",
       name: "HUMALIT",
       type: "Catalogue Website",
-      language: "Vue.js",
       summary:
         "HUMALIT Group 3 is an article and reflection catalogue website that was used as the final project for a non CS class.",
-      tags: ["Vue", "BootStrap", "Responsive", "Agile", "Wireframe"],
       features: [
         "Static website that is used as the final output for an non CS class.",
         "The site contains articles and analysis relating to the given theme.",
         "When creating the site, using vue and bootstrap, agile methodology and wireframes were used.",
       ],
+      tags: ["Vue", "Bootstrap", "Responsive"],
       isAddress: true,
       address: "https://michikogo.github.io/HUMALIT-FinalOutput/#/",
     },
@@ -54,19 +46,113 @@ const Education = () => {
       language: "ReactJs/ Firebase",
       summary:
         "MermaidSpa is an ecommerce site that users can buy products, check their services and promos. This site was used practice my react and firebase skills.",
-      tags: ["React", "react-bootstrap", "Responsive", "Agile", "Wireframe"],
       features: [
         "Simple responsive website that allows users to check products, services, and make reservations.",
         "The website was created to practice react and react-bootstrap, and learn firebase.",
       ],
-      isAddress: false,
+      tags: ["React", "react-bootstrap", "Responsive"],
+      isAddress: true,
       address: "https://michikogo.github.io/The-Mermaid-Spa-and-Bath/#/",
+    },
+    {
+      id: 4,
+      image: "projectImage.PNG",
+      name: "Task Tracker",
+      type: "Freelanced Web Application",
+      summary: "",
+      tags: ["HTML", "CSS", "jQuery", "Bootstrap"],
+      features: [
+        "To do list tracker with features such as add, edit, delete and filter a task and showing duration of task.",
+        "The site was commissioned by a lowerbatch who needed help with creating web applications.",
+      ],
+      isAddress: false,
+      address: "",
+    },
+  ];
+  const mobileProjects = [
+    {
+      id: 1,
+      image: "projectImage.PNG",
+      name: "Car Sitter",
+      type: "Mobile Application",
+      summary: "",
+      tags: ["Andriod Studio", "Java"],
+      features: [
+        "Created a mobile application where people can store their car information and alert them if they have to do their yearly car checkup.",
+      ],
+      isAddress: false,
+      address: "",
+    },
+  ];
+  const otherProjects = [
+    {
+      id: 1,
+      image: "projectImage.PNG",
+      name: "Basketball Shot Analysis",
+      type: "Undergraduate Thesis",
+      summary: "",
+      features: [
+        "Created a tool to check the basketball shot form of a player when shooting either a three point or free throw.",
+        "The project consisted of a camera, editing software, tensorflow, python and machine learning such as CNN and SVM.",
+        "The goal of the project is to find the best shooting form to get a successful shot.",
+      ],
+      tags: ["Python", "Machine Learning", "Preprocessing", "OpenPose"],
+      isAddress: false,
+      address: "",
+    },
+    {
+      id: 2,
+      image: "projectImage.PNG",
+      name: "Color Match",
+      type: "Game Development",
+      summary: "",
+      features: [
+        "A game with 30 levels that players must go through the gates with the same color as the player progresses the level gets harder and faster.",
+        "The game requires languages such as C# and Unity to create objects, shaders and point systems.",
+        "The game was showcased in during a school event for students to give feedback and rate the game.",
+      ],
+      tags: ["Unity", "C+"],
+      isAddress: false,
+      address: "",
+    },
+    {
+      id: 3,
+      image: "projectImage.PNG",
+      name: "Calendar View",
+      type: "Java",
+      summary: "",
+      tags: ["Python", "Machine Learning"],
+      features: [
+        "Created a tool to check the basketball shot form of a player when shooting either a three point or free throw.",
+        "The project consisted of a camera, editing software, tensorflow, python and machine learning such as CNN and SVM.",
+        "The goal of the project is to find the best shooting form to get a successful shot.",
+      ],
+      isAddress: false,
+      address: "",
+    },
+    {
+      id: 4,
+      image: "projectImage.PNG",
+      name: "Pokemon Fishing",
+      type: "Java",
+      summary: "",
+      features: [
+        "The game consisted of a player moving each block using W,A,S,D keys then fishing to see which pokemon they caught.",
+        "The project was in compliance with a CS subject that enhances students java skills and MVC framework.",
+      ],
+      tags: ["Java", "Machine Learning"],
+      isAddress: false,
+      address: "",
     },
   ];
 
   return (
     <Container fluid className="project-background">
-      <ProjectContent projects={projects} />
+      <ProjectContent
+        webProjects={webProjects}
+        mobileProjects={mobileProjects}
+        otherProjects={otherProjects}
+      />
     </Container>
   );
 };
