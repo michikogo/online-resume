41fc60ca69a063f259627f4e5babdb11a35c1e3f
michikogo <37466954+michikogo@users.noreply.github.com>
2021-04-12 03:47:50 +0800

    added data

--- src/Sections/Project/Project.js
+++ src/Sections/Project/Project.js
@@ -13,8 +13,8 @@ const Education = () => {
       summary:
         "BakeryMNL is a simple responsive site that uses ReactJs and JSON Server. This site eCommerce site was created to enhance my skills in both React Hooks and JSON Server.",
       features: [
-        "Simple responsive website that allows users to buy products, show their total bill, add more products and place their information for shipping or pickup",
-        "The website was created to practice react and react-bootstrap, and JSON server.",
+        "Add, edit and delete can be done in cart page using JSON server ",
+        "On bakery page each item's description can be seen and items can be added to cart",
       ],
       tags: ["React", "JSON Server", "react-bootstrap", "Responsive"],
       isAddress: true,
@@ -24,15 +24,14 @@ const Education = () => {
       id: 2,
       image: "HUMALIT.PNG",
       name: "HUMALIT",
-      type: "Catalogue Website",
+      type: "Catalogue Application",
       summary:
-        "HUMALIT Group 3 is an article and reflection catalogue website that was used as the final project for a non CS class.",
+        "Static website that was created as the final output for an non CS class. The site contains articles and analysis relating to the given theme.",
       features: [
-        "Static website that is used as the final output for an non CS class.",
-        "The site contains articles and analysis relating to the given theme.",
-        "When creating the site, using vue and bootstrap, agile methodology and wireframes were used.",
+        "When creating the site agile method was used for the progress to be seen and tested for errors.",
+        "Wireframes were made to speed up the development.",
       ],
-      tags: ["Vue", "Bootstrap", "Responsive"],
+      tags: ["Vue", "vue-bootstrap", "Responsive"],
       isAddress: true,
       address: "https://michikogo.github.io/HUMALIT-FinalOutput/#/",
     },
@@ -40,13 +39,14 @@ const Education = () => {
       id: 3,
       image: "mermaidSpa.PNG",
       name: "The Mermaid and Spa",
-      type: "Business Website",
+      type: "Business Application",
       language: "ReactJs/ Firebase",
       summary:
-        "MermaidSpa is an ecommerce site that users can buy products, check their services and promos. This site was used practice my react and firebase skills.",
-      features: [
         "Simple responsive website that allows users to check products, services, and make reservations.",
-        "The website was created to practice react and react-bootstrap, and learn firebase.",
+      features: [
+        "Using firebase, each product was pulled from the database to be shown on the webpage.",
+        "Using firebase, reservations are reflected on the database.",
+        "Using react and react-bootstrap, the design of the website was created.",
       ],
       tags: ["React", "react-bootstrap", "Responsive"],
       isAddress: true,
@@ -56,43 +56,65 @@ const Education = () => {
       id: 4,
       image: "projectImage.PNG",
       name: "Task Tracker",
-      type: "Freelanced Web Application",
-      summary: "",
+      type: "Freelanced Application",
+      summary:
+        "The site was commissioned by a student who needed help with creating web applications.",
       tags: ["HTML", "CSS", "jQuery", "Bootstrap"],
       features: [
         "To do list tracker with features such as add, edit, delete and filter a task and showing duration of task.",
-        "The site was commissioned by a lowerbatch who needed help with creating web applications.",
+        "The project prohibited the use of databases, so features were done using jQuery.",
+      ],
+      isAddress: false,
+      address: "",
+    },
+    {
+      id: 5,
+      image: "projectImage.PNG",
+      name: "Simple Instagram",
+      type: "Social Network Application",
+      summary:
+        "Simple react website that is similar to instagram with features such as browsing through profiles, hearting pictures and commenting on them.",
+      tags: ["React", "ant-design"],
+      features: [
+        "Using react, when hearting a picture the icon can change from black to pink.",
+        "Using react and other npm packages, carousels were created to browse through profiles.",
+        "There was no backend in this project since this was the first react project, with this ant design was explored.",
       ],
       isAddress: false,
       address: "",
     },
   ];
+
   const mobileProjects = [
     {
       id: 1,
       image: "projectImage.PNG",
       name: "Car Sitter",
       type: "Mobile Application",
-      summary: "",
-      tags: ["Andriod Studio", "Java"],
+      summary:
+        "Mobile application where users can store their car information and alert them for their yearly car check up.",
+      tags: ["Andriod Studio", "SQLite"],
       features: [
-        "Created a mobile application where people can store their car information and alert them if they have to do their yearly car checkup.",
+        "Android studio was used as the base environment for this development, with it all of the design and connections to the database were made possible.",
+        "SQLite was used as the main database for users to store their car information.",
       ],
       isAddress: false,
       address: "",
     },
   ];
+
   const otherProjects = [
     {
       id: 1,
       image: "projectImage.PNG",
       name: "Basketball Shot Analysis",
       type: "Undergraduate Thesis",
-      summary: "",
+      summary:
+        "Created a tool to check the best machine learning algorithm to detect the best basketball shooting form of a player for both three point or free throws. The project consisted of a camera, editing software, tensorflow, python and machine learning such as CNN and SVM. The project consisted of 4 members. I handled feature extraction, preprocessing and machine learning.",
       features: [
-        "Created a tool to check the basketball shot form of a player when shooting either a three point or free throw.",
-        "The project consisted of a camera, editing software, tensorflow, python and machine learning such as CNN and SVM.",
-        "The goal of the project is to find the best shooting form to get a successful shot.",
+        "In feature extraction, I extracted the X and Y coordinates of the player.",
+        "In preprocessing, I computed the angles and lengths given the coordinates, cleaned the rows with no values and affine transformed the players for all of them to be in uniformed height.",
+        "In machine learning, I used SVM and KNN to compare which machine algorithm is the best to detect the best basketball shooting form using its accuracy.",
       ],
       tags: ["Python", "Machine Learning", "Preprocessing", "OpenPose"],
       isAddress: false,
@@ -103,13 +125,13 @@ const Education = () => {
       image: "projectImage.PNG",
       name: "Color Match",
       type: "Game Development",
-      summary: "",
-      features: [
+      summary:
         "A game with 30 levels that players must go through the gates with the same color as the player progresses the level gets harder and faster.",
+      features: [
         "The game requires languages such as C# and Unity to create objects, shaders and point systems.",
         "The game was showcased in during a school event for students to give feedback and rate the game.",
       ],
-      tags: ["Unity", "C+"],
+      tags: ["Unity", "C#"],
       isAddress: false,
       address: "",
     },
@@ -118,12 +140,12 @@ const Education = () => {
       image: "projectImage.PNG",
       name: "Calendar View",
       type: "Java",
-      summary: "",
+      summary:
+        "Calendar view that had features namely adding, editing, deleting and notifying the user. The project was in compliance with a CS subject that requires students to use agile, waterfall and scrum methodologies.",
       tags: ["Python", "Machine Learning"],
       features: [
-        "Created a tool to check the basketball shot form of a player when shooting either a three point or free throw.",
-        "The project consisted of a camera, editing software, tensorflow, python and machine learning such as CNN and SVM.",
-        "The goal of the project is to find the best shooting form to get a successful shot.",
+        "The project consisted of a team of 9 members and each member had a module to work on and at the end of each week the project is compiled for the testers to check if there are any errors in the project.",
+        "In the project, I handled adding and editing features, which allows users to add and edit the date, time, event name and duration of notifying the user.",
       ],
       isAddress: false,
       address: "",
@@ -133,12 +155,13 @@ const Education = () => {
       image: "projectImage.PNG",
       name: "Pokemon Fishing",
       type: "Java",
-      summary: "",
+      summary:
+        "The game consisted of a player moving each block using W,A,S,D keys then fishing to see which pokemon they caught. The project was in compliance with a CS subject that enhances students' java skills and MVC framework.",
       features: [
-        "The game consisted of a player moving each block using W,A,S,D keys then fishing to see which pokemon they caught.",
-        "The project was in compliance with a CS subject that enhances students java skills and MVC framework.",
+        "In the game, the players coordinate and the place they're going to move to were computed to show that the player moved in the GUI.",
+        "When fishing, a random number will be produced to indicate the pokemon that was caught.",
       ],
-      tags: ["Java", "Machine Learning"],
+      tags: ["Java", "Java GUI"],
       isAddress: false,
       address: "",
     },
