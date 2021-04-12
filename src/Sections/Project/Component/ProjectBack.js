c097ab88c5f6b0a6524efd231d6fee4b22279a1a
michikogo <37466954+michikogo@users.noreply.github.com>
2021-04-13 04:13:57 +0800

    Update ProjectBack.js

--- src/Sections/Project/Component/ProjectBack.js
+++ src/Sections/Project/Component/ProjectBack.js
@@ -3,67 +3,69 @@ import { Row, Col, Image, Badge } from "react-bootstrap";
 
 const ProjectBack = ({ project }) => {
   return (
-    <>
-      <Row>
-        {/* TITLE & TYPE*/}
-        <Col>
-          <p className="project-back-name">{project.name}</p>
-          <p className="project-back-type">{project.type}</p>
-        </Col>
-      </Row>
-      {/* SUMMARY */}
-      <Row>
-        <Col>
-          <p className="project-back-summary">{project.summary}</p>
-        </Col>
-      </Row>
-      {/* FEATURES */}
-      <Row>
-        <Col>
-          <p className="project-back-section-title">Features:</p>
-        </Col>
-      </Row>
-      {project.features.map((feature) => (
-        <Row key={feature}>
-          <Col xs={2} lg={1} className="project-back-bullet-image-col">
-            <Image
-              fluid
-              alt="arrowLeft"
-              src={require(`../../../Assets/Image/arrowLeft.png`).default}
-            />
-          </Col>
+    <Row className="project-back-row">
+      <Col>
+        <Row>
+          {/* TITLE & TYPE*/}
           <Col>
-            <p className="project-back-bullet">{feature}</p>
+            <p className="project-back-name">{project.name}</p>
+            <p className="project-back-type">{project.type}</p>
           </Col>
         </Row>
-      ))}
-      {/* LANGAUGE/LIBRARIES */}
-      <Row>
-        <Col className="project-back-section-title">Languages/Libraries:</Col>
-      </Row>
-      <Row className="project-back-pill-row">
-        {project.tags.map((tag) => (
-          <Col key={tag}>
-            <Badge variant="primary">{tag}</Badge>
+        {/* SUMMARY */}
+        <Row>
+          <Col>
+            <p className="project-back-summary">{project.summary}</p>
           </Col>
+        </Row>
+        {/* FEATURES */}
+        <Row>
+          <Col>
+            <p className="project-back-section-title">Features:</p>
+          </Col>
+        </Row>
+        {project.features.map((feature) => (
+          <Row key={feature}>
+            <Col xs={2} lg={1} className="project-back-bullet-image-col">
+              <Image
+                fluid
+                alt="arrowLeft"
+                src={require(`../../../Assets/Image/arrowLeft.png`).default}
+              />
+            </Col>
+            <Col>
+              <p className="project-back-bullet">{feature}</p>
+            </Col>
+          </Row>
         ))}
-      </Row>
-      {/* BUTTON REROUTE */}
-      <Row className="project-back-button">
-        {project.isAddress && (
-          <a href={project.address} target="_block">
-            <button className="custom-btn btn-8">
-              <span>Launch</span>
+        {/* LANGAUGE/LIBRARIES */}
+        <Row>
+          <Col className="project-back-section-title">Languages/Libraries:</Col>
+        </Row>
+        <Row className="project-back-pill-row">
+          {project.tags.map((tag) => (
+            <Col key={tag}>
+              <Badge variant="primary">{tag}</Badge>
+            </Col>
+          ))}
+        </Row>
+        {/* BUTTON REROUTE */}
+        <Row className="project-back-button">
+          {project.isAddress && (
+            <a href={project.address} target="_block">
+              <button className="custom-btn btn-8">
+                <span>Launch</span>
+              </button>
+            </a>
+          )}
+          {!project.isAddress && (
+            <button className="custom-btn custom-button-disabled">
+              <span>Out of Service</span>
             </button>
-          </a>
-        )}
-        {!project.isAddress && (
-          <button className="custom-btn custom-button-disabled">
-            <span>Out of Service</span>
-          </button>
-        )}
-      </Row>
-    </>
+          )}
+        </Row>
+      </Col>
+    </Row>
   );
 };
 
